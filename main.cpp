d0aa9dae06dbf2d5fa575804956d1e9af8c5d311
Dr-42 <royspandan1@gmail.com>
2022-06-13 23:01:12 +0530

    Struct based material handling

--- main.cpp
+++ main.cpp
@@ -153,8 +153,6 @@ int main()
 	// -----------
 	while (!glfwWindowShouldClose(window))
 	{
-		// per-frame time logic
-		// --------------------
 		float currentFrame = static_cast<float>(glfwGetTime());
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
@@ -165,16 +163,31 @@ int main()
 
 		// render
 		// ------
-		glClearColor(0.01f, 0.01f, 0.01f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// be sure to activate shader when setting uniforms/drawing objects
 		lightingShader.use();
-		lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
-		lightingShader.setVec3("lightColor",  1.0f, 1.0f, 1.0f);
-
-		lightingShader.setVec3("lightPos", lightPos);
+		lightingShader.setVec3("light.position", lightPos);
 		lightingShader.setVec3("viewPos", camera.Position);
+
+		// light properties
+		glm::vec3 lightColor;
+		lightColor.x = static_cast<float>(sin(glfwGetTime() * 2.0));
+		lightColor.y = static_cast<float>(sin(glfwGetTime() * 0.7));
+		lightColor.z = static_cast<float>(sin(glfwGetTime() * 1.3));
+		glm::vec3 diffuseColor = lightColor   * glm::vec3(0.5f); // decrease the influence
+		glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f); // low influence
+		lightingShader.setVec3("light.ambient", ambientColor);
+		lightingShader.setVec3("light.diffuse", diffuseColor);
+		lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
+		// material properties
+		lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+		lightingShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+		lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f); // specular lighting doesn't have full effect on this object's material
+		lightingShader.setFloat("material.shininess", 32.0f);
+
 		// view/projection transformations
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		glm::mat4 view = camera.GetViewMatrix();
@@ -183,9 +196,6 @@ int main()
 
 		// world transformation
 		glm::mat4 model = glm::mat4(1.0f);
-		//rotate the cube around the y axis over time
-		model = glm::rotate(model, glm::radians(static_cast<float>(glfwGetTime()) * -10.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-
 		lightingShader.setMat4("model", model);
 
 		// render the cube
--- shaders/colors.frag
+++ shaders/colors.frag
@@ -1,33 +1,45 @@
-#version 330 core
+ #version 330 core
 out vec4 FragColor;
 
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPos;
-uniform vec3 viewPos;
+struct Material {
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;	
+	float shininess;
+}; 
 
-in vec3 FragPos;
-in vec3 Normal;
+struct Light {
+	vec3 position;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+in vec3 FragPos;  
+in vec3 Normal;  
+  
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
 
 void main()
 {
-	float ambientStrength = 0.1;
-	float specularStrength = 0.5;
-	float diffuseStrength = 0.5;
-	
-	vec3 ambient = ambientStrength * lightColor;
-
+	// ambient
+	vec3 ambient = light.ambient * material.ambient;
+  	
+	// diffuse 
 	vec3 norm = normalize(Normal);
-	vec3 lightDir = normalize(lightPos - FragPos);
-
+	vec3 lightDir = normalize(light.position - FragPos);
 	float diff = max(dot(norm, lightDir), 0.0);
-	vec3 diffuse = diff * lightColor * diffuseStrength;
-
+	vec3 diffuse = light.diffuse * (diff * material.diffuse);
+	
+	// specular
 	vec3 viewDir = normalize(viewPos - FragPos);
-	vec3 reflectDir = reflect(-lightDir, norm);
-	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 128);
-	vec3 specular = specularStrength * spec * lightColor;
+	vec3 reflectDir = reflect(-lightDir, norm);  
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+	vec3 specular = light.specular * (spec * material.specular);  
 
-	vec3 result = (ambient + diffuse + specular) * objectColor;
+	vec3 result = ambient + diffuse + specular;
 	FragColor = vec4(result, 1.0);
-}
+} 
